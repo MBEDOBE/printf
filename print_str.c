7232c4c3e65070c1e7c66394c8312caf0df555a3
D. Mbedobe <danielbushbaby@gmail.com>
2022-10-18 14:29:09 +0000

    add

--- print_str.c
+++ print_str.c
@@ -23,8 +23,7 @@ int print_str(va_list args, char *buffer)
 		return (_print_buf(null, 6));
 	}
 
-	for (; buffer[i] != '\0'; i++)
-		;
+	for (; buffer[i] != '\0'; i++);
 
 	return (_print_buf(buffer, i));
 }
