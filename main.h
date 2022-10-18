9b4c6df4f9d29aa3b26634ada7cf79d577cd884a
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-10-18 15:14:17 +0000

    Update main.h

--- main.h
+++ main.h
@@ -35,6 +35,6 @@ int pnt_plus_int(va_list, char *buffer);
 int pnt_space_int(va_list, char *buffer);
 int pnt_o_octal(va_list, char *buffer);
 int pnt_upp_hex(va_list, char *buffer);
-char *rot13(va_list list);
+int  prnt_rot13(va_list, char list);
 
 #endif
